ac4ccd6d8b3caed0d0413bb5c6512e208f0bb508
Sebastian Hjelm <sebastian.hjelm@apptus.com>
2020-07-29 10:59:47 +0200

    Add autocrlf setting to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 set -x
 git config --global pull.rebase true
 git config --global core.editor "vim"
+git config --global core.autocrlf input
 
 printf "\n\n" >> ~/.bashrc
 printf ". \"$(pwd)/.bash_aliases\"" >> ~/.bashrc
