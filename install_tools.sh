cd36074e54aec7423f539232113ef103209d61d5
Sebastian Hjelm <melon.bag@hotmail.com>
2023-09-15 00:04:42 +0200

    Make tool installation support Manjaro

--- install_tools.sh
+++ install_tools.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-# Ubuntu install
-sudo apt install -y git gitk fonts-firacode yakuake scrot
+is_ubuntu() {
+    if [ -n "$(grep "Ubuntu" /etc/os-release)" ]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+}
+
+if [ is_ubuntu ]; then
+    sudo apt install -y git gitk fonts-firacode yakuake scrot xclip
+else
+    sudo pacman -S git tk ttf-fira-code xclip
+fi
 sudo snap install --classic nvim
