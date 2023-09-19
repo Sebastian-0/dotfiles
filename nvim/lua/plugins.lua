852b95bfbf49fa5f507ba3d98deac411eede6108
Dummy <dummy@intuicell.com>
2023-09-19 10:15:16 +0200

    Fix version number

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -15,7 +15,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.4',
+        tag = '0.1.3',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
     {
