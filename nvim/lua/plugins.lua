45981f6cf6ae2659aeea3f8011905014fc9df120
Sebastian Hjelm <sebastian@intuicell.com>
2023-09-19 16:33:01 +0200

    Add vim plugins for scrolling and comments
    Also reorganize a bit

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -16,16 +16,23 @@ require("lazy").setup({
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.3',
-        dependencies = { 'nvim-lua/plenary.nvim' }
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        end
     },
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config= function()
+        config = function()
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
-                ensure_installed = { "c", "cpp", "lua", "vim", "python", "typescript", "bash", "javascript", "rust" },
+                ensure_installed = { "c", "cpp", "cmake", "lua", "vim", "python", "typescript", "tsx", "bash", "javascript", "rust" },
                 sync_install = false,
                 highlight = { enable = true },
 --                indent = { enable = true },  
@@ -36,15 +43,21 @@ require("lazy").setup({
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000
+    },
+    {
+        "karb94/neoscroll.nvim",
+        opts = {
+            stop_eof = false,
+            easing_function = "quadratic"
+        }
+    },
+    {
+        "terrortylor/nvim-comment",
+        config = function()
+            require("nvim_comment").setup()
+        end
     }
 })
 
 -- Enable theme
 vim.cmd.colorscheme "catppuccin"
-
--- Configure Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
