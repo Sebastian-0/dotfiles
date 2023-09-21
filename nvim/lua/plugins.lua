a07b5db5c19fa9735b0cf089f7227a66c39a2de8
Sebastian Hjelm <melon.bag@hotmail.com>
2023-09-22 00:01:52 +0200

    Customize lualine

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -67,7 +67,14 @@ require("lazy").setup({
             require("lualine").setup {
                 options = {
                     theme = "catppuccin"
-                }
+                },sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch', 'diff', 'diagnostics'},
+                    lualine_c = {'filename'},
+                    lualine_x = {'os.date("%d %b %H:%M")', 'encoding', 'fileformat', 'filetype'},
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'}
+                },
             }
         end
     },
