282e7efcb45928b6d995508cad084e1f8a98f9a5
Sebastian Hjelm <sebastian@intuicell.com>
2023-10-23 15:07:46 +0200

    Add install script for the remote server command

---
+++ remote_server/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eou pipefail
+
+read -sp Enter server password: PASS
+echo $PASS > pass
+chmod 600 pass
+
+sudo apt-get install -y terminator
+mkdir ~/.config/terminator
+cp terminator_config ~/.config/terminator/config 
+
+cat <<EOF >> ~/.bashrc
+
+function remote_server_workspace() (
+    cd $PWD
+    ./launch_server_workspace.sh
+)
+EOF
