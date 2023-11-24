40b8414a4262d2e6cbc2547849b36bea40dde197
Sebastian Hjelm <sebastian@intuicell.com>
2023-11-24 16:06:52 +0100

    Add gitsigns plugin to vim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -152,6 +152,21 @@ require("lazy").setup({
         end
     },
     {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require('gitsigns').setup({
+                current_line_blame = true,
+                current_line_blame_opts = {
+                    delay = 500
+                },
+                on_attach = function(_)
+                    local gs = package.loaded.gitsigns
+                    vim.keymap.set('n', '<leader>hd', gs.diffthis)
+                end
+            })
+        end
+    },
+    {
         "folke/neodev.nvim",
         opts = {},
         lazy = false,
