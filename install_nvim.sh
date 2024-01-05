c8b231036cedc658221c1ae906aba7a09a23ed58
Sebastian Hjelm <sebastian@intuicell.com>
2024-01-05 14:55:31 +0100

    Set editor alternatives to nvim on Ubuntu

--- install_nvim.sh
+++ install_nvim.sh
@@ -15,6 +15,10 @@ echo "Install Neovim..."
 sudo snap install --classic nvim
 if [ "$(is_ubuntu)" = "true" ]; then
     sudo apt-get install -y ripgrep xclip fd-find python3-venv npm
+    sudo update-alternatives --install /usr/bin/editor editor $(which nvim) 100
+    if [ -f ~/.selected_editor ]; then
+        echo SELECTED_EDITOR="\"$(which nvim)\"" > ~/.selected_editor
+    fi
 else
     sudo pacman -S --needed --noconfirm ripgrep xclip fd npm
 fi
