d9aa38f26e0b22c3a062ce2242e72e31f182781e
Sebastian Hjelm <sebastian@intuicell.com>
2024-04-25 14:08:37 +0200

    Add mesh-6 to remote_server_workspace

--- remote_server/launch_server_workspace.sh
+++ remote_server/launch_server_workspace.sh
@@ -25,17 +25,18 @@ launch_mesh() {
     i3-msg "exec terminator -x $dir/launch_ssh.sh /tmp/remote_pass mesh-3"
     i3-msg "exec terminator -x $dir/launch_ssh.sh /tmp/remote_pass mesh-4"
     i3-msg "exec terminator -x $dir/launch_ssh.sh /tmp/remote_pass mesh-5"
+    i3-msg "exec terminator -x $dir/launch_ssh.sh /tmp/remote_pass mesh-6"
 }
 
 if [ $# = 0 ]; then
-    i3-msg "append_layout $dir/server_workspace_17.json"
+    i3-msg "append_layout $dir/server_workspace_18.json"
     launch_workers
     launch_mesh
 elif [ "$1" == "--worker" ] || [ "$1" == "-w" ]; then
     i3-msg "append_layout $dir/server_workspace_4.json"
     launch_workers
 elif [ "$1" == "--mesh" ] || [ "$1" == "-m" ]; then
-    i3-msg "append_layout $dir/server_workspace_5.json"
+    i3-msg "append_layout $dir/server_workspace_6.json"
     launch_mesh
 elif [ "$1" == "--core" ] || [ "$1" == "-c" ]; then
     i3-msg "append_layout $dir/server_workspace_1.json"
