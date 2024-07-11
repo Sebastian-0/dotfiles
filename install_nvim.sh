dded277915b7b55b7fb2bb2863e71e82cb3c1e04
Sebastian Hjelm <sebastian@intuicell.com>
2024-07-11 10:16:24 +0200

    Add shellcheck to vim install
    This improves the quality of bashls

--- install_nvim.sh
+++ install_nvim.sh
@@ -18,13 +18,13 @@ if [ "$(which npm)" ]; then
     npm=
 fi
 if [ "$(is_ubuntu)" = "true" ]; then
-    sudo apt-get install -y ripgrep xclip fd-find python3-venv $npm
+    sudo apt-get install -y ripgrep xclip fd-find python3-venv shellcheck $npm
     sudo update-alternatives --install /usr/bin/editor editor $(which nvim) 100
     if [ -f ~/.selected_editor ]; then
         echo SELECTED_EDITOR="\"$(which nvim)\"" > ~/.selected_editor
     fi
 else
-    sudo pacman -S --needed --noconfirm ripgrep xclip fd $npm
+    sudo pacman -S --needed --noconfirm ripgrep xclip fd shellcheck $npm
 fi
 git config --global core.editor "nvim"
 
