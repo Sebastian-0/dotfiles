69586c742f6b371ca3f7e6d650f9e50790809a3e
Sebastian Hjelm <sebastian@intuicell.com>
2024-08-09 10:52:02 +0200

    Support defining which polybar to use in .xprofile

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,8 +6,11 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
+# Set this value in ~/.xprofile or /etc/environment
+bar=${BAR_NAME:-base}
+
 echo "---" | tee -a /tmp/polybar.log
-polybar 2>&1 | tee -a /tmp/polybar.log &
+polybar -c ~/.config/polybar/config.ini "$bar" 2>&1 | tee -a /tmp/polybar.log &
 disown
 
 echo "Bar launched..."
