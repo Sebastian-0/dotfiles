0c1fcaf8ea29b65d094cadbdb738acea3058eedd
Sebastian Hjelm <sebastian@intuicell.com>
2025-03-04 16:43:02 +0100

    Add autosession plugin as a trial

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -326,6 +326,7 @@ require("lazy").setup({
             })
         end
     },
+    {'rmagatti/auto-session', commit = '9c3f977', lazy = false, opts = {}},
     {"karb94/neoscroll.nvim", commit = 'f957373', opts = {stop_eof = false, easing_function = "quadratic"}},
     {"ryanoasis/vim-devicons", version = '0.11.x'},
     {"lambdalisue/suda.vim", version = "1.2.x"},
