0b425382da0e80bbfa4eb3d52d709945850d8714
Sebastian Hjelm <melon.bag@hotmail.com>
2025-08-15 09:28:29 +0200

    Restructure vim installation

--- install_nvim.sh
+++ install_nvim.sh
@@ -12,22 +12,41 @@ if ! which nvim >&/dev/null; then
     if which npm >&/dev/null; then
         npm=
     fi
+    # NOTE:
+    # - `luarocks` is required by lazy.nvim (for rockspec) and luaformatter
+    # - `python3-pynvim` is required for jupyter-vim (I think?)
+    # - `xclip` is required for clipboard support in vim
+    # - `ripgrep` and `fd-find` are required by telescope.nvim
+    # - `python3-venv` is required by ???
+    # - `npm` is required by nvim-treesitter (I think?)
     if is_ubuntu; then
-        sudo apt-get install -y --no-install-recommends ripgrep xclip fd-find python3-venv shellcheck python3-pynvim gcc cmake ninja-build g++ luarocks $npm
+        sudo apt-get install -y --no-install-recommends ripgrep xclip fd-find python3-venv python3-pynvim luarocks $npm
         sudo update-alternatives --install /usr/bin/editor editor "$(which nvim)" 100
         if [ -f ~/.selected_editor ]; then
             echo SELECTED_EDITOR="\"$(which nvim)\"" > ~/.selected_editor
         fi
     elif is_arch; then
-        sudo pacman -S --needed --noconfirm ripgrep xclip fd shellcheck python-pynvim gcc cmake ninja luarocks $npm
+        sudo pacman -S --needed --noconfirm ripgrep xclip fd python-virtualenv python-pynvim luarocks $npm
     else
         echo "Unsupported OS!"
         exit 1
     fi
 fi
 
+echo "Install LSP dependencies..."
+# NOTE:
+# - `shellcheck` is needed by bashls for linting
+# - `gcc`, `c++`, `cmake` and `ninja` are required by glslls
+if is_ubuntu; then
+    sudo apt-get install -y --no-install-recommends shellcheck gcc cmake ninja-build g++
+elif is_arch; then
+    sudo pacman -S --needed --noconfirm shellcheck gcc cmake ninja
+else
+    echo "Unsupported OS!"
+    exit 1
+fi
+
 echo "Install formatters..."
-sudo luarocks install --server=https://luarocks.org/dev luaformatter
 if is_ubuntu; then
     sudo apt-get install -y --no-install-recommends shfmt
 elif is_arch; then
@@ -36,6 +55,7 @@ else
     echo "Unsupported OS!"
     exit 1
 fi
+sudo luarocks install --server=https://luarocks.org/dev luaformatter
 
 echo "Configure nvim..."
 symlink_config nvim
