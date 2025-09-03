cc1b363dadcb2fb212b3f0b02953520ee7970d92
Sebastian Hjelm <melon.bag@hotmail.com>
2025-09-03 23:01:17 +0200

    Start making more advanced snippets for zig

--- nvim/lua/snippets/zig.lua
+++ nvim/lua/snippets/zig.lua
@@ -1,4 +1,13 @@
 local luasnip = require("luasnip")
 local s = luasnip.snippet
 local t = luasnip.text_node
-return {s("all", t("allocator: std.mem.Allocator"))}
+local i = luasnip.insert_node
+-- TODO:
+-- * Snippets with newline?
+-- * Override existing snippets?
+-- * Import already defined s, t, i, etc... from luasnip instead of defining our own?
+return {
+    s("all", t("allocator: std.mem.Allocator")),
+    s("aall", t("allocator: *std.heap.ArenaAllocator")),
+    s("fri", {t("for (0.."), i(1, "", {}), t(") |"), i(2, "i", {}), t("| {"), i(3, "", {}), t("}")})
+}
